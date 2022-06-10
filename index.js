a34d0f81824ca538a3bc025e5066435e74ffb18a
AlinsAlians <omondia6@gmail.com>
2022-06-10 18:07:42 +0300

    nothing changed

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+// a recursive function that loops through an array and displays
+// an element after a given period of time.
 function slideShow(index){
     let items = ["a","b","c","d"];
     if(index>items.length-1){
